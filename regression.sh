c5d0f9c16652bbb6faa23ab64c20171f3e841616
Deepak Achariya <deepak.achariya@gmail.com>
2022-05-16 12:39:34 -0700

    Updated regression.sh for phase_tests

--- regression.sh
+++ regression.sh
@@ -20,3 +20,17 @@ mv sim.log SIMULATION_LOGS/drv10975_eeprom_test.log
 ./simv top.sv +UVM_TESTNAME=i2c_wr_on_ro_reg_test | tee sim.log
 mv sim.log SIMULATION_LOGS/i2c_wr_on_ro_reg_test.log
 
+./simv top.sv +UVM_TESTNAME=phase_accl_to_duty_cycle_test | tee sim.log
+mv sim.log SIMULATION_LOGS/phase_accl_to_duty_cycle_test.log
+
+./simv top.sv +UVM_TESTNAME=phase_change_dirn_test | tee sim.log
+mv sim.log SIMULATION_LOGS/phase_change_dirn_test.log
+
+./simv top.sv +UVM_TESTNAME=phase_lower_spd_2_zero_test | tee sim.log
+mv sim.log SIMULATION_LOGS/phase_lower_spd_2_zero_test.log
+
+./simv top.sv +UVM_TESTNAME=phase_speedup_wo_accln_test | tee sim.log
+mv sim.log SIMULATION_LOGS/phase_speedup_wo_accln_test.log
+
+./simv top.sv +UVM_TESTNAME=phase_startup_w_accln_test | tee sim.log
+mv sim.log SIMULATION_LOGS/phase_startup_w_accln_test.log
